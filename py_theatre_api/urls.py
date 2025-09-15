ad4ae7e53a64f9413fde320b95084c9973d2aa9a
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-15 19:19:31 +0300

    linked application endpoints to the main ulrs.py

--- py_theatre_api/urls.py
+++ py_theatre_api/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/theatre/",
+        include("theatre.urls", namespace="theatre")
+    ),
+    path(
+        "api/bookings/",
+        include("bookings.urls", namespace="bookings")
+    ),
+    path(
+        "api/users/",
+        include("accounts.urls", namespace="accounts")
+    )
 ]
