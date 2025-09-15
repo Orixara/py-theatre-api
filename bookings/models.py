8f7aee12dedb079815b68f4745d0e529e6ceeabf
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-15 17:33:15 +0300

    created models for bookings app and made migrations

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,84 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from theatre.models import Performance
+
+
+User = get_user_model()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name="reservations", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Reservation"
+        verbose_name_plural = "Reservations"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Reservation - ({self.id}: {self.created_at})"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    seat = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    performance = models.ForeignKey(
+        Performance,
+        related_name="tickets",
+        on_delete=models.CASCADE,
+    )
+    reservation = models.ForeignKey(
+        "Reservation",
+        related_name="reservation_tickets",
+        on_delete=models.CASCADE
+    )
+
+    @staticmethod
+    def validate_tickets(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row")
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_tickets(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            *args,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    class Meta:
+        verbose_name = "Ticket"
+        verbose_name_plural = "Tickets"
+        unique_together = [["performance", "row", "seat"]]
+        ordering = ["row", "seat"]
+
+    def __str__(self):
+        return f"Ticket: ({self.row}, {self.seat})"
