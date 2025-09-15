c9c2e503de66ce41cbfad47ad1b4e5bdaf3d1a12
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-15 19:33:32 +0300

    added property for actor's full name and serializers for genre, actor and theatre hall

---
+++ theatre/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from theatre.models import Actor, Genre, TheatreHall, Play, Performance
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row")
