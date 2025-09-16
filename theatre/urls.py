96a918b1fea1168cb1cfdd13487ddff2f9e18cbd
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-16 16:37:53 +0300

    created common modelviewsets for genre, actor and theatre_hall and registered routers

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path, include
+from rest_framework import routers
 
+from theatre.views import (
+    GenreViewSet,
+    ActorViewSet,
+    TheatreHallViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("theatre_hall", TheatreHallViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
 
 app_name = "theatre"
-
-
-urlpatterns = []
--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-# Create your views here.
+from py_theatre_api.permissions import IsAdminOrIfAuthenticatedReadOnly
+from theatre.models import Genre, Actor, TheatreHall
+from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+
+class ActorViewSet(ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+
+class TheatreHallViewSet(ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
