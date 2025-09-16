f9473a3ca1ade9461ec8c63f06f727e4778f8ecf
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-16 20:41:20 +0300

    Created views for Play and Performance models without filtration

--- theatre/urls.py
+++ theatre/urls.py
@@ -4,13 +4,17 @@ from rest_framework import routers
 from theatre.views import (
     GenreViewSet,
     ActorViewSet,
-    TheatreHallViewSet
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet
 )
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("theatre_hall", TheatreHallViewSet)
+router.register("play", PlayViewSet)
+router.register("performance", PerformanceViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,19 @@
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.viewsets import ModelViewSet
+from django.db.models import Count, F
 
 from py_theatre_api.permissions import IsAdminOrIfAuthenticatedReadOnly
-from theatre.models import Genre, Actor, TheatreHall
-from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
+from theatre.models import Genre, Actor, TheatreHall, Play, Performance
+from theatre.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    TheatreHallSerializer,
+    PlaySerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
+    PerformanceSerializer,
+    PerformanceListSerializer,
+    PerformanceDetailSerializer
+)
 
 
 class GenreViewSet(ModelViewSet):
@@ -22,3 +32,53 @@ class TheatreHallViewSet(ModelViewSet):
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+
+class PlayViewSet(ModelViewSet):
+    queryset = Play.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+        return PlaySerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ["list", "retrieve"]:
+            queryset = queryset.prefetch_related("actors", "genres")
+        return queryset
+
+
+class PerformanceViewSet(ModelViewSet):
+    queryset = Performance.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PerformanceListSerializer
+        if self.action == "retrieve":
+            return PerformanceDetailSerializer
+        return PerformanceSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            queryset = (
+                queryset
+                .select_related("play", "theatre_hall")
+                .annotate(
+                    tickets_available=(
+                        F("theatre_hall__rows") * F("theatre_hall__seats_in_row")
+                    )  - Count("tickets")
+                )
+                .order_by("show_time")
+            )
+        elif self.action == "retrieve":
+            queryset = queryset.select_related(
+                "play",
+                "theatre_hall"
+            ).prefetch_related("tickets")
+        return queryset
