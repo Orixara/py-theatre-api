40fcb62c0db20e49a89b159cbee5e8d4b6db154c
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-16 19:53:40 +0300

    created endpoints for app accounts, with profile and JWT token authentification

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from accounts.serializers import (
+    LoginSerializer,
+    UserRegistrationSerializer,
+    UserProfileSerializer,
+    ChangePasswordSerializer
+)
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+    throttle_classes = [AnonRateThrottle]
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+    throttle_classes = [AnonRateThrottle]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(
+            {
+                "access": serializer.validated_data["access"],
+                "refresh": serializer.validated_data["refresh"],
+            }
+        )
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class ChangePasswordView(generics.GenericAPIView):
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [IsAuthenticated]
+    throttle_classes = [UserRateThrottle]
+
+    def post(self, request):
+        serializer = self.get_serializer(
+            data=request.data,
+            context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+
+        user = request.user
+        user.set_password(serializer.validation_data["new_password"])
+        user.save()
+
+        return Response(
+            {"message": "Password updated successfully."}
+        )
